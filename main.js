a620927210ed9a0dc0df2d9d12f8081c71a451f2
mojila <mojila@github.com>
2018-01-24 14:21:18 +0700

    Teori fix

--- main.js
+++ main.js
@@ -25,3 +25,20 @@ var cek_semua_praktikum = () => {
     option.checked = true;
   }
 }
+var cek_semua_teori = () => {
+  // A
+  for (var i = 0; i < 9; i++) {
+    option = document.getElementById('a'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // B
+  for (var i = 9; i < 14; i++) {
+    option = document.getElementById('b'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // C
+  for (var i = 0; i < 2; i++) {
+    option = document.getElementById('e'+(i+1)+'_2');
+    option.checked = true;
+  }
+}
