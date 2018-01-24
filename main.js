2558529ca284418e61c18969ec4a8dd4aa453baa
mojila <mojila@github.com>
2018-01-24 14:13:00 +0700

    Praktikum

---
+++ main.js
@@ -0,0 +1,27 @@
+var cek_semua = () => {
+  // A
+  for (var i = 0; i < 3; i++) {
+    option = document.getElementById('a'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // B
+  for (var i = 0; i < 7; i++) {
+    option = document.getElementById('b'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // C
+  for (var i = 0; i < 5; i++) {
+    option = document.getElementById('c'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // D
+  for (var i = 0; i < 3; i++) {
+    option = document.getElementById('d'+(i+1)+'_9');
+    option.checked = true;
+  }
+  // E
+  for (var i = 0; i < 2; i++) {
+    option = document.getElementById('e'+(i+1)+'_2');
+    option.checked = true;
+  }
+}
